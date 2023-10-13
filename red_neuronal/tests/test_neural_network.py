96e0b1c8841e939cf711e2ad1a596bd09f58f6dc
Andrés Fernández <anfernandez@fi.uba.ar>
2023-10-13 13:57:53 -0300

    [WIP] adds new tests for neural network

--- red_neuronal/tests/test_neural_network.py
+++ red_neuronal/tests/test_neural_network.py
@@ -1,11 +1,13 @@
-from django.test import TestCase
+import os
+import random
 import pandas as pd
+from django.test import TestCase
+from django.conf import settings
 
 # Project
 from red_neuronal.components.neural_network import NeuralNetwork
 from red_neuronal.tests.utils.faker import create_fake_df
-
-# Create your tests here.
+from red_neuronal.utils.exceptions.exceptions import UntrainedNeuralNetwork
 
 
 class NeuralNetworkTestCase(TestCase):
@@ -20,8 +22,58 @@ class NeuralNetworkTestCase(TestCase):
             "titulo": "short_text",
             "anio_exp": "year",
         }
+        self.remove_persistence_files()
 
-    def test_neural_network(self):
+    def create_fit_df(self, training_df: pd.DataFrame, fit_df_length=100):
+        votes = list(training_df["voto"].unique())
+        fit_votes = [random.choice(votes) for _ in range(fit_df_length)]
+        legislators = list(training_df["diputado_nombre"].unique())
+        fit_legislators = [random.choice(legislators) for _ in range(fit_df_length)]
+        parties = list(training_df["partido"].unique())
+        fit_parties = [random.choice(parties) for _ in range(fit_df_length)]
+        df_columns = {
+            "ley": "int",
+            "expediente_inicial": "exp",
+            "texto": "text",
+            "titulo": "short_text",
+            "anio_exp": "year",
+        }
+        fit_df = create_fake_df(df_columns, fit_df_length, as_dict=False)
+        fit_df["voto"] = fit_votes
+        fit_df["diputado_nombre"] = fit_legislators
+        fit_df["partido"] = fit_parties
+        return fit_df
+
+    def remove_persistence_files(self):
+        files = os.listdir(settings.MODEL_SAVING_DIR)
+        # Iterate through the files and remove them
+        for file in files:
+            file_path = os.path.join(settings.MODEL_SAVING_DIR, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
+    def test_neural_network_training(self):
         df: pd.DataFrame = create_fake_df(self.COLUMNS, n=500, as_dict=False)
-        neural_network = NeuralNetwork(df)
-        neural_network.compile()
+        neural_network = NeuralNetwork()
+        neural_network.train(df)
+
+    def test_neural_network_fit_with_in_memory_modely(self):
+        train_df: pd.DataFrame = create_fake_df(self.COLUMNS, n=500, as_dict=False)
+        fit_df: pd.DataFrame = self.create_fit_df(train_df)
+        neural_network = NeuralNetwork()
+        neural_network.train(train_df)
+        neural_network.fit(fit_df)
+
+    def test_neural_network_fit_with_persisted_model(self):
+        train_df: pd.DataFrame = create_fake_df(self.COLUMNS, n=500, as_dict=False)
+        fit_df: pd.DataFrame = self.create_fit_df(train_df)
+        neural_network = NeuralNetwork()
+        neural_network.train(train_df)
+        neural_network = NeuralNetwork()
+        neural_network.fit(fit_df)
+
+    def test_neural_network_fitting_without_training_raises_exception(self):
+        df: pd.DataFrame = create_fake_df(self.COLUMNS, n=100, as_dict=False)
+        neural_network = NeuralNetwork()
+        with self.assertRaises(UntrainedNeuralNetwork):
+            neural_network.fit(df)
---
+++ red_neuronal/utils/exceptions/exceptions.py
@@ -0,0 +1,53 @@
+# rest_framework
+from rest_framework.exceptions import APIException
+
+
+class CustomException(APIException):
+    def __init__(self, code: str, status_code: int, description: str):
+        self.status_code = status_code  # needed for DRF to work
+        self.detail = {
+            "code": code,
+            "status_code": status_code,
+            "description": description,
+        }
+
+    def __str__(self):
+        info = "\n"
+        for key, value in self.detail.items():
+            if key != "extra_details":
+                info += f"{key}: {value}\n"
+            else:
+                if value:
+                    info += f"{key}:\n"
+                    for key, value in value.items():
+                        info += f"\t{key}: {value}\n"
+        return info
+
+
+class UntrainedNeuralNetwork(CustomException):
+    def __init__(self):
+        code = "UNTRAINED_NEURAL_NETWORK"
+        status_code = 400
+        description = "The neural network has not been trained yet."
+        super().__init__(code, status_code, description)
+
+
+class EncodingException(CustomException):
+    class Meta:
+        abstract = True
+
+
+class EncoderDataNotFound(EncodingException):
+    def __init__(self):
+        code = "ENCODER_DATA_NOT_FOUND"
+        status_code = 400
+        description = "The encoder data was not found. You must train the neural network before fitting it."
+        super().__init__(code, status_code, description)
+
+
+class TransformingUnseenData(EncodingException):
+    def __init__(self):
+        code = "TRANSFORMING_UNSEEN_DATA"
+        status_code = 400
+        description = "The data the encoder is trying to transform has values that were not present in the fitted data."
+        super().__init__(code, status_code, description)
